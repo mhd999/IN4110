5bbf6ea98ee037b530f5f08554ee6f9a54bfe50b
Mohamad al moualem <m.almouallem@gmail.com>
2019-09-25 10:02:33 +0200

    Feat: improve output format

--- assignment3/wc.py
+++ assignment3/wc.py
@@ -1,24 +1,29 @@
 import sys
+import ntpath
 
 def countNumberOfWordsInFile(file):
     f = open(file, "r")
     numberOfWords = f.read().split()
-    print(len(numberOfWords))
+    print('number of words in a file: {wordsNo}'.format(wordsNo=len(numberOfWords)))
 
 def countNumberOfLinesInFile(file):
     f = open(file, "r")
     nummberOflines = sum(1 for line in f)
-    print(nummberOflines)
+    print('number of lines in a file: {lineNo}'.format(lineNo=nummberOflines))
 
 
 def countNumberOfCharsInFile(file):
     text1=open(file,'r').read()
-    print(len(text1))
+    print('number of chars in a file: {charNo}'.format(charNo=len(text1)))
+
+def printFileName(file):
+    print('name of the file: {fileName}'.format(fileName=ntpath.basename(file)))
 
 def main(argv):
     countNumberOfWordsInFile(argv[0])
     countNumberOfLinesInFile(argv[0])
     countNumberOfCharsInFile(argv[0])
+    printFileName(argv[0])
   
   
 if __name__ == '__main__':
